7f83f664119c135009c42124b2ae2d79b85af432
Gokila-K2593 <gokilakannan2003@gmail.com>
2025-02-21 19:32:56 +0530

    files are added

--- question1.py
+++ question1.py
@@ -1,4 +1,4 @@
-with open('file1.txt', 'r') as source_file, open('file2.txt', 'w') as destination_file:
+with open('File1.txt','r') as source_file,open('File2.txt','w') as destination_file:
     content = source_file.read()
     destination_file.write(content)
 print("File copied successfully!")
