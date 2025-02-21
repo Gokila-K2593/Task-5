05b03e542408afd2fc506ea5fd172e1bf86f12b7
Gokila-K2593 <gokilakannan2003@gmail.com>
2025-02-21 19:18:23 +0530

    files are added

--- question1.py
+++ question1.py
@@ -1,9 +1,4 @@
-# Open file1.txt in read mode ('r') and file2.txt in write mode ('w')
 with open('file1.txt', 'r') as source_file, open('file2.txt', 'w') as destination_file:
-    # Read the contents of file1.txt
     content = source_file.read()
-    
-    # Write the contents to file2.txt
     destination_file.write(content)
-
 print("File copied successfully!")
